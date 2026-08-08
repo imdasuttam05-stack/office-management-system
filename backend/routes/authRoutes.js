5418c9361ab28541f3e5b39f550946dda0d4677a
imdasuttam05-stack <imdas.uttam05@gmail.com>
2026-08-08 15:22:42 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { sendOtp } from "../controllers/authController.js";
+
+import {
+  sendOtp,
+  verifyOtp,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
+// Send OTP
+// POST /api/auth/send-otp
 router.post("/send-otp", sendOtp);
+
+// Verify OTP
+// POST /api/auth/verify-otp
+router.post("/verify-otp", verifyOtp);
 
 export default router;
