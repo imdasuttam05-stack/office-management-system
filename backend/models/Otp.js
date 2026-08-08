ef37334012d5c147ec29e15c877bbadfaccef636
imdasuttam05-stack <imdas.uttam05@gmail.com>
2026-08-08 13:41:50 +0530

    Create Otp.js

---
+++ backend/models/Otp.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const otpSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    otp: {
+      type: String,
+      required: true,
+    },
+
+    expiresAt: {
+      type: Date,
+      required: true,
+      index: {
+        expires: 0,
+      },
+    },
+
+    attempts: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Otp", otpSchema);
