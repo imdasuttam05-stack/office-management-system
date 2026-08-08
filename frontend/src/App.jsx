b1f8211f8500fccdfa99aae6e17b39beb5b8cb9f
imdasuttam05-stack <imdas.uttam05@gmail.com>
2026-08-08 14:17:04 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
+import Login from "./pages/Login.jsx";
+
 function App() {
-  return (
-    <div>
-      <h1>Office Management System</h1>
-      <p>Frontend Connected Successfully</p>
-    </div>
-  );
+  return <Login />;
 }
 
 export default App;
