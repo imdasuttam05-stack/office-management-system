c9a558a985ae4dbcbd9fea64cad255bb31ef4f78
imdasuttam05-stack <imdas.uttam05@gmail.com>
2026-08-08 13:43:41 +0530

    Create authController.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,42 @@
+import User from "../models/User.js";
+import Otp from "../models/Otp.js";
+
+import { generateOtp } from "../utils/generateOtp.js";
+import { sendOtpEmail } from "../services/emailService.js";
+
+export const sendOtp = async (req, res) => {
+  try {
+    const email = req.body.email?.trim().toLowerCase();
+
+    if (!email) {
+      return res.status(400).json({
+        success: false,
+        message: "Email is required",
+      });
+    }
+
+    const otp = generateOtp();
+
+    await Otp.deleteMany({ email });
+
+    await Otp.create({
+      email,
+      otp,
+      expiresAt: new Date(Date.now() + 5 * 60 * 1000),
+    });
+
+    await sendOtpEmail(email, otp);
+
+    return res.status(200).json({
+      success: true,
+      message: "OTP sent successfully",
+    });
+  } catch (error) {
+    console.error("Send OTP Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Unable to send OTP",
+    });
+  }
+};
