ff572ea62df60934f75764b65d805288dcef8082
imdasuttam05-stack <imdas.uttam05@gmail.com>
2026-08-08 18:33:12 +0530

    Create ProtectedRoute.jsx

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,33 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+export default function ProtectedRoute({ allowedRoles }) {
+  const token = localStorage.getItem("token");
+  const userData = localStorage.getItem("user");
+
+  // Not logged in
+  if (!token || !userData) {
+    return <Navigate to="/login" replace />;
+  }
+
+  let user;
+
+  try {
+    user = JSON.parse(userData);
+  } catch {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    return <Navigate to="/login" replace />;
+  }
+
+  // Role permission
+  if (
+    allowedRoles &&
+    allowedRoles.length > 0 &&
+    !allowedRoles.includes(user.role)
+  ) {
+    return <Navigate to="/dashboard" replace />;
+  }
+
+  return <Outlet />;
+}
