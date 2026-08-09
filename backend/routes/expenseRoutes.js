958d12d6dcc6d0d00a4417fbdb36f2904e08ba43
imdasuttam05-stack <imdas.uttam05@gmail.com>
2026-08-09 22:52:29 +0530

    Update expenseRoutes.js

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -11,12 +11,18 @@ import {
 } from "../controllers/expenseController.js";
 
 import auth from "../middleware/auth.js";
+import requireRole from "../middleware/role.js";
 
 const router = express.Router();
 
 router.post(
   "/",
   auth,
+  requireRole(
+    "Admin",
+    "Manager",
+    "Employee"
+  ),
   createExpense
 );
 
@@ -41,18 +47,27 @@ router.put(
 router.delete(
   "/:id",
   auth,
+  requireRole("Admin"),
   deleteExpense
 );
 
 router.patch(
   "/:id/approve",
   auth,
+  requireRole(
+    "Admin",
+    "Manager"
+  ),
   approveExpense
 );
 
 router.patch(
   "/:id/reject",
   auth,
+  requireRole(
+    "Admin",
+    "Manager"
+  ),
   rejectExpense
 );
 
